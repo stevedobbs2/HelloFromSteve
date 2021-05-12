e8445d747f9f9cf9cd2e9b0746dc6bdcd2bda8ef
ManDrewCat <84036827+ManDrewCat@users.noreply.github.com>
2021-05-12 16:08:24 -0600

    Update Main.java
    Forgot the ;

--- src/dobbs/Main.java
+++ src/dobbs/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Hello, my name is Steve");
         System.out.println("Hello Steve, my name is Ricardo!");
-	System.out.println("Hello Steve, my name is Andrew!")
+	System.out.println("Hello Steve, my name is Andrew!");
     }
 }
